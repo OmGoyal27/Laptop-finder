80a5fd4a4f41e959656071a00c9716ae945dfa08
OmGoyal27 <omgoyal2711@gmail.com>
2024-05-09 21:55:02 +0530

    Delete testing.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-new_path = Path("commands.txt")
-command = new_path.read_text(encoding='utf-8')
-print(command)
-commands_path = Path("log.txt")
-commands_path.write_text('utf-8')
