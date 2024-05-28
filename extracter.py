1e525d07945070eb64fb6b9cffd400fbdf7d58ae
OmGoyal27 <omgoyal2711@gmail.com>
2024-05-28 20:12:16 +0530

    Add files via upload

--- extracter.py
+++ extracter.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 commands_path = Path("commands.txt")
-commands = commands_path.read_text(encoding='utf-8')
+commands = commands_path.read_text(encoding='utf-8')            # Extracts all the commands
